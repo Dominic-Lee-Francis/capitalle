e97ee2fefc45de0f936474a656111152abef6d9d
Dominic-Lee-Francis <dominic.l.francis@outlook.com>
2024-03-24 16:59:03 +0800

    Restarted due to errors.
    Will begin the services and router again.

--- server/router/userRouter.js
+++ server/router/userRouter.js
@@ -1,18 +1,1 @@
-class UserRouter {
-  constructor(express, userService) {
-    this.router = express.Router();
-    this.userService = userService;
-  }
 
-  router() {
-    let router = this.express.Router();
-
-    router.get("/", async (req, res) => {
-      const users = await this.userService.getAllUsers();
-      res.json(users);
-    });
-    return router;
-  }
-}
-
-module.exports = UserRouter;
