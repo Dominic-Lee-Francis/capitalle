2e4bfd5fc17b79ea01b484a14a60f6307c5fac4c
Dominic-Lee-Francis <dominic.l.francis@outlook.com>
2024-03-28 19:36:03 +0800

    Added authenticate, if user is logged in, they can connect.

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,47 @@
+// CSS
 import "./App.css";
+// Components
 import Navbar from "./Components/Navbar";
+// Pages
 import Home from "./Pages/Home";
 import Login from "./Pages/Login";
 import Statistics from "./Pages/Statistics";
 import Rules from "./Pages/Rules";
 import Contact from "./Pages/Contact";
 import PleaseLogin from "./Pages/PleaseLogin";
+// React
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 function App() {
-  const user = false;
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const getUser = async () => {
+      fetch("http://localhost:8080/auth/login/success", {
+        method: "GET",
+        credentials: "include",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Credentials": true,
+        },
+      })
+        .then((response) => {
+          if (response.status === 200) return response.json();
+          throw new Error("failed to authenticate user");
+        })
+        .then((resObject) => {
+          setUser(resObject.user);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    };
+    getUser();
+  }, []);
+
+  console.log(user);
 
   return (
     <BrowserRouter>
