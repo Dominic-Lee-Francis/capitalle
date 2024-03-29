bd0678f5664c0e68d3355c9a85d0535fb4da15b7
Dominic-Lee-Francis <dominic.l.francis@outlook.com>
2024-03-29 11:04:06 +0800

    Added onSubmit to register a user
    Register User

--- client/src/Pages/Register.jsx
+++ client/src/Pages/Register.jsx
@@ -9,11 +9,27 @@ const Register = () => {
   const [error, setError] = useState(false);
   const [confirmPassword, setConfirmPassword] = useState("");
 
+  const onSubmitForm = async (e) => {
+    e.preventDefault();
+    try {
+      const body = { username, email, password };
+      const response = await fetch("http://localhost:8080/auth/register", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(body),
+      });
+      const parseRes = await response.json();
+      console.log(parseRes);
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
   return (
     <div className="register">
       <h1 className="registerTitle">Create an Account</h1>
       <div className="wrapper">
-        <form className="centre">
+        <form className="centre" onSubmit={onSubmitForm}>
           <input
             type="text"
             placeholder="Username"
