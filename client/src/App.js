13c090adc7a5baa6e5cabf93743d9acced9fe269
Dominic-Lee-Francis <dominic.l.francis@outlook.com>
2024-04-02 19:25:06 +0800

    installed redux

--- client/src/App.js
+++ client/src/App.js
@@ -16,6 +16,30 @@ import { useEffect, useState } from "react";
 function App() {
   const [user, setUser] = useState(null);
 
+  const [country, setCountry] = useState(null);
+  useEffect(() => {
+    const getCountry = async () => {
+      fetch("http://localhost:8080/capital", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+        .then((response) => {
+          if (response.status === 200) return response.json();
+          throw new Error("failed to authenticate user");
+        })
+        .then((resObject) => {
+          setCountry(resObject);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    };
+    getCountry();
+  }, []); // Add an empty dependency array to run the effect only once
+  console.log(country);
+
   useEffect(() => {
     // Fetch the user data from the server
     const getUser = async () => {
@@ -43,30 +67,6 @@ function App() {
   }, []);
 
   console.log(user);
-
-  const [country, setCountry] = useState(null);
-  useEffect(() => {
-    const getCountry = async () => {
-      fetch("http://localhost:8080/capital", {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      })
-        .then((response) => {
-          if (response.status === 200) return response.json();
-          throw new Error("failed to authenticate user");
-        })
-        .then((resObject) => {
-          setCountry(resObject);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-    };
-    getCountry();
-  }, []); // Add an empty dependency array to run the effect only once
-  console.log(country);
 
   return (
     <BrowserRouter>
